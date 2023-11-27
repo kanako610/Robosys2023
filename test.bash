e43739d5f7cfdf88a22fcac4d88cd9790e0d6971
Kanako Takahashi <jerryt6103@gmail.com>
2023-11-28 00:18:32 +0900

    更新

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Kanako Takahashi
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,9 +9,19 @@ ng(){
 
 res=0
 
-###I/O TEST###
-out=$(seq 5 |./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+	###I/O TEST###
+	out=$(seq 5 |./plus)
+	[ "${out}" = 15 ] || ng ${LINENO}
 
-["$res" = 0 ] && echo OK
+
+	### STRANGE INPUT ###
+	out=$(echo あ　| ./plus)
+	[ "$?" = 1 ]     || ng ${LINENO}
+	[ "${out}" = "" ] || ng ${LINENO}
+
+	out=$(echo |./plus)
+	[ "$?" = 1 ]     || ng${LINENO}
+	[ "${out}" = "" ] || ng${LINENO}
+
+[ "$res" = 0 ] && echo OK
 exit $res
