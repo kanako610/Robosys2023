82ad6f71149d05429508896dba12acc20b87022b
Kanako Takahashi <jerryt6103@gmail.com>
2023-11-29 01:33:03 +0900

    更新

--- test.bash
+++ test.bash
@@ -23,5 +23,13 @@ res=0
 	[ "$?" = 1 ]     || ng${LINENO}
 	[ "${out}" = "" ] || ng${LINENO}
 
+
+	###Plus2 INPUT###
+	out=$(seq 5 |./plus2)
+        [ "${out}" = '足し算の答えは: 15.0
+掛け算の答えは: 120.0
+最大の数値は: 5.0
+最小の数値は: 1.0' ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
